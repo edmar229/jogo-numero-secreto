201e240fad339cd4066c532cfc27dfe2f7cf92d8
Edmar <edmarvieira003@gmail.com>
2024-06-30 10:03:19 -0300

    Desafio: hora da prática

---
+++ desafios/array/lista-linguagens.js
@@ -0,0 +1,20 @@
+// Criando uma lista vazia
+const listaGenerica = []
+
+// Criando uma lista de linguagens de programação
+const linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python']
+
+// Adicionando elementos à lista de linguagens de programação
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang')
+
+// Criando uma lista com 3 nomes
+const nomes = ['Ana', 'Carlos', 'Beatriz']
+
+// Exibindo o primeiro elemento da lista 'nomes'
+console.log(nomes[0]) // Resultado: Ana
+
+// Exibindo o segundo elemento da lista 'nomes'
+console.log(nomes[1]) // Resultado: Carlos
+
+// Exibindo o último elemento da lista 'nomes'
+console.log(nomes[nomes.length - 1]) // Resultado: Beatriz
